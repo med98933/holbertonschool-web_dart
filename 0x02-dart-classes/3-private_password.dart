24aba5c5e455c93018b79dae131461f1d8f974b4
med98933 <m.ladhari@outlook.com>
2022-12-27 17:05:44 +0100

    add passeword

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,13 @@
+class Password {
+    String _password;
+  Password({required String password}) : _password = password;
+  String get password => _password;
+
+  bool isValid() {
+    return ((_password.length >= 8) &&
+    (_password.length <= 16)) && (this._password.contains(RegExp(r'^[a-zA-Z\d]*$')));
+  }
+
+  @override
+  String toString() => 'Your Password is: ${this._password}';
+}
