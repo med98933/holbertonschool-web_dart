72cc5b13ce00e983e57d58d0acc01372cbe72323
med98933 <m.ladhari@outlook.com>
2022-12-27 17:43:31 +0100

    update

--- 0x02-dart-classes/4-mutables.dart
+++ 0x02-dart-classes/4-mutables.dart
@@ -11,6 +11,7 @@ class Password {
     return ((_password.length >= 8) &&(_password.length <= 16))
      && (this.password.contains(new RegExp(r"[A-Z]"))&& this.password.contains(RegExp(r"[a-z]"))
 	&& this.password.contains( RegExp(r"[0-9]")));
+  }
 
   @override
   String toString() => 'Your Password is: ${this._password}';
