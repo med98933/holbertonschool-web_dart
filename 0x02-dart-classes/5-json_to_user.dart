4e6a1f8ea634fe194d550237a67e1b894f590fcb
med98933 <m.ladhari@outlook.com>
2022-12-29 19:03:38 +0100

    add json to user

---
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -0,0 +1,29 @@
+class User {
+  int id;
+  String name;
+  int age;
+  double height;
+  User(
+      {required this.id,
+      required this.name,
+      required this.age,
+      required this.height});
+
+  Map toJson() => {
+        'id': id,
+        'name': name,
+        'age': age,
+        'height': height,
+      };
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return User(
+        id: userJson['id'],
+        name: userJson['name'],
+        age: userJson['age'],
+        height: userJson['height']);
+  }
+
+  String toString() {
+    return "User(id : $id ,name: $name, age: $age, height: $height)";
+  }
+}
